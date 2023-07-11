fd7e8b776f64cd1c08bc92d319231a396bef36e9
Hugo <hugo.quitton@gmail.com>
2023-07-11 11:15:32 +0200

    Added, enemy target player

--- Assets/Scripts/Systems/EnemyMovementDirSystem.cs
+++ Assets/Scripts/Systems/EnemyMovementDirSystem.cs
@@ -3,22 +3,39 @@ using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 using Unity.Burst;
+using Unity.Transforms;
+using Unity.Mathematics;
 
 [BurstCompile]
 public partial struct EnemyMovementDirSystem : ISystem 
 {
+
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        foreach (RefRW<MovementComponent> enemy in SystemAPI.Query<RefRW<MovementComponent>>())
+        foreach (var enemy in SystemAPI.Query<RefRW<MovementComponent>, RefRO<EnemyTag>, RefRO<LocalTransform>>())
         {
-            RandomDir(ref state, enemy);
+            PlayerDir(ref state,enemy.Item1,enemy.Item3.ValueRO.Position);
         }
     }
 
-    private void RandomDir(ref SystemState state, RefRW<MovementComponent> mc)
+    private float3 FindPlayer(ref SystemState state)
     {
-        mc.ValueRW.dir = new Vector2(Random.Range(-1, 2), Random.Range(-1, 2));
+        foreach (var player in SystemAPI.Query<RefRO<LocalTransform>, RefRO<PlayerTag>>())
+        {
+            return player.Item1.ValueRO.Position;
+        }
+        return float3.zero;
+    }
+
+    private void PlayerDir(ref SystemState state, RefRW<MovementComponent> enemy, float3 enemyPos)
+    {
+        Vector3 dir = (FindPlayer(ref state) - enemyPos);
+        dir.Normalize();
+        dir *= enemy.ValueRO.speed;
+
+        enemy.ValueRW.dir = new Vector2(dir.x,dir.z);
     }
 
 }
